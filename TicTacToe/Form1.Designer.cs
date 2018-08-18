4fd08ee008b6f3b0071311f87b0ac027491dc4f3
DESKTOP-8U12EAU\Jaimie Ouwerling <ouwerling.jaimie@gmail.com>
2018-08-17 23:26:00 -0200

    Uploading my first project - TicTacToe

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TicTacTow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Playe1_Button = new System.Windows.Forms.Button();
+            this.Player2_button = new System.Windows.Forms.Button();
+            this.Start_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Playe1_Button
+            // 
+            this.Playe1_Button.BackColor = System.Drawing.Color.Cyan;
+            this.Playe1_Button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Playe1_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Playe1_Button.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Playe1_Button.Location = new System.Drawing.Point(645, 97);
+            this.Playe1_Button.Name = "Playe1_Button";
+            this.Playe1_Button.Size = new System.Drawing.Size(150, 76);
+            this.Playe1_Button.TabIndex = 0;
+            this.Playe1_Button.Text = "Player1 -- X";
+            this.Playe1_Button.UseVisualStyleBackColor = false;
+            this.Playe1_Button.Click += new System.EventHandler(this.Playe1_Button_Click);
+            // 
+            // Player2_button
+            // 
+            this.Player2_button.BackColor = System.Drawing.Color.MediumPurple;
+            this.Player2_button.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.Player2_button.FlatAppearance.BorderSize = 10;
+            this.Player2_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2_button.ForeColor = System.Drawing.Color.Indigo;
+            this.Player2_button.Location = new System.Drawing.Point(645, 227);
+            this.Player2_button.Name = "Player2_button";
+            this.Player2_button.Size = new System.Drawing.Size(150, 76);
+            this.Player2_button.TabIndex = 1;
+            this.Player2_button.Text = "Player2 -- O";
+            this.Player2_button.UseVisualStyleBackColor = false;
+            this.Player2_button.Click += new System.EventHandler(this.Player2_button_Click);
+            // 
+            // Start_button
+            // 
+            this.Start_button.BackColor = System.Drawing.Color.LightGreen;
+            this.Start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start_button.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Start_button.Location = new System.Drawing.Point(633, 355);
+            this.Start_button.Name = "Start_button";
+            this.Start_button.Size = new System.Drawing.Size(177, 98);
+            this.Start_button.TabIndex = 2;
+            this.Start_button.Text = "Start!!!";
+            this.Start_button.UseVisualStyleBackColor = false;
+            this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(220, 474);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 45);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(955, 567);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start_button);
+            this.Controls.Add(this.Player2_button);
+            this.Controls.Add(this.Playe1_Button);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Playe1_Button;
+        private System.Windows.Forms.Button Player2_button;
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacTow
+{
+    public partial class Form1 : Form
+    {
+        private bool win = false;
+        private int[,] arr = new int[3,3];
+        private int i = new int();
+        private int j = new int();
+        private int count = new int();
+        private int row = new int();
+        private bool player1 = true;
+        private bool player2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(Color.Blue); 
+            g.DrawLine(p,120,170,490,170);
+            g.DrawLine(p,120,330,490,330);
+            g.DrawLine(p,220,70,220,430);
+            g.DrawLine(p,390,70,390,430);
+            Playe1_Button.BackColor = Color.DarkCyan;
+            Playe1_Button.ForeColor = Color.Cyan;
+            Array.Clear(arr, 0, 9);//מחיקה של מערך,(כמות התאים למחיקה, מיקום התחלתי במערך, שם המערך) שש
+            count = 0;
+            player1 = true;
+            player2 = false;
+            win = false;
+            textBox1.Text = "";
+        }
+
+        private void Player2_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Playe1_Button_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!win)
+            {
+                if (e.X >= 120 && e.X <= 220 && e.Y >= 70 && e.Y <= 170 && arr[0, 0] == 0) //שמאל למעלה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(140, 150, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[0, 0] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 140, 150, 140 + 50, 150 - 50);
+                        g.DrawLine(p, 140, 150 - 50, 140 + 50, 150);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[0, 0] = 1;
+                    }
+                }
+                else if (e.X >= 390 && e.X <= 490 && e.Y >= 70 && e.Y <= 170 && arr[0, 2] == 0) // ימין למעלה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(410, 150, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[0, 2] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 410, 150, 410 + 50, 150 - 50);
+                        g.DrawLine(p, 410, 150 - 50, 410 + 50, 150);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[0, 2] = 1;
+                    }
+                }
+                else if (e.X >= 120 && e.X <= 220 && e.Y >= 330 && e.Y <= 430 && arr[2, 0] == 0)// שמאל למטה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(140, 410, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[2, 0] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 140, 410, 140 + 50, 410 - 50);
+                        g.DrawLine(p, 140, 410 - 50, 140 + 50, 410);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[2, 0] = 1;
+                    }
+                }
+                else if (e.X >= 390 && e.X <= 490 && e.Y >= 330 && e.Y <= 430 && arr[2, 2] == 0)// ימין למטה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(410, 410, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[2, 2] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 410, 410, 410 + 50, 410 - 50);
+                        g.DrawLine(p, 410, 410 - 50, 410 + 50, 410);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[2, 2] = 1;
+                    }
+                }
+                else if (e.X >= 120 && e.X <= 220 && e.Y >= 170 && e.Y <= 330 && arr[1, 0] == 0) // שמאל אמצע
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(140, 280, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[1, 0] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 140, 280, 140 + 50, 280 - 50);
+                        g.DrawLine(p, 140, 280 - 50, 140 + 50, 280);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[1, 0] = 1;
+                    }
+                }
+                else if (e.X >= 390 && e.X <= 490 && e.Y >= 170 && e.Y <= 330 && arr[1, 2] == 0)// ימין אמצע
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(410, 280, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[1, 2] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 410, 280, 410 + 50, 280 - 50);
+                        g.DrawLine(p, 410, 280 - 50, 410 + 50, 280);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[1, 2] = 1;
+                    }
+                }
+                else if (e.X >= 220 && e.X <= 390 && e.Y >= 70 && e.Y <= 170 && arr[0, 1] == 0)// אמצע למעלה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(280, 150, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[0, 1] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 280, 150, 280 + 50, 150 - 50);
+                        g.DrawLine(p, 280, 150 - 50, 280 + 50, 150);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[0, 1] = 1;
+                    }
+                }
+                else if (e.X >= 220 && e.X <= 390 && e.Y >= 330 && e.Y <= 430 && arr[2, 1] == 0)// אמצע למטה
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(280, 410, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[2, 1] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 280, 410, 280 + 50, 410 - 50);
+                        g.DrawLine(p, 280, 410 - 50, 280 + 50, 410);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[2, 1] = 1;
+                    }
+                }
+                else if (e.X >= 220 && e.X <= 390 && e.Y >= 170 && e.Y <= 330 && arr[1, 1] == 0)// אמצע
+                {
+                    if (player2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.MediumPurple);
+                        Rectangle r = new Rectangle(280, 275, 60, -60);
+                        g.DrawEllipse(p, r);
+                        player2 = false;
+                        player1 = true;
+                        Playe1_Button.BackColor = Color.DarkCyan;
+                        Playe1_Button.ForeColor = Color.Cyan;
+                        Player2_button.BackColor = Color.MediumPurple;
+                        Player2_button.ForeColor = Color.Indigo;
+                        arr[1, 1] = 2;
+                    }
+                    else if (player1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Pen p = new Pen(Color.Cyan);
+                        g.DrawLine(p, 280, 275, 280 + 50, 275 - 50);
+                        g.DrawLine(p, 280, 275 - 50, 280 + 50, 275);
+                        player2 = true;
+                        player1 = false;
+                        Playe1_Button.BackColor = Color.Cyan;
+                        Playe1_Button.ForeColor = Color.DarkCyan;
+                        Player2_button.BackColor = Color.Indigo;
+                        Player2_button.ForeColor = Color.MediumPurple;
+                        arr[1, 1] = 1;
+                    }
+                }
+
+
+                for (i = 0; i < 3; i++)
+                {
+
+                    if (arr[i, j] == arr[i, j + 1] && arr[i, j] == arr[i, j + 2] && arr[i, j] != 0)
+                    {
+                        win = true;
+                        break;
+                    }
+                    count++;
+
+                    if (arr[row, i] == arr[row + 1, i] && arr[row, i] == arr[row + 2, i] && arr[row, i] != 0)
+                    {
+                        win = true;
+                        break;
+                    }
+                    count++;
+
+                    if (count < 5)
+                    {
+                        if (arr[i, j] == arr[i + 1, j + 1] && arr[i, j] == arr[i + 2, j + 2] && arr[i, j] != 0)
+                        {
+                            win = true;
+                            break;
+                        }
+
+                        count++;
+                    }
+                    if (count < 5)
+                    {
+                        if (arr[i, j + 2] == arr[i + 1, j + 1] && arr[i, j + 2] == arr[i + 2, j] && arr[i, j + 2] != 0)
+                        {
+                            win = true;
+                            break;
+                        }
+                        if (count < 5)
+                            count++;
+                    }
+                }
+            }
+            if(win)
+            {
+                if(!player1)
+                {
+                    textBox1.Text = "Player 1 Wins!!!";
+                    textBox1.ForeColor = Color.Cyan;
+                }
+                if(!player2)
+                {
+                    textBox1.Text = "Player 2 Wins!!!";
+                    textBox1.ForeColor = Color.MediumPurple;
+                }
+                Graphics g = this.CreateGraphics();
+                Pen p1 = new Pen(Color.Red);
+                switch (count)
+                {
+                    case 0: g.DrawLine(p1, 120, 120, 490, 120);//שורה למעלה
+                        break;
+                    case 1: g.DrawLine(p1, 170, 70, 170, 430);//טור שמאל
+                        break;
+                    case 2: g.DrawLine(p1, 120, 70, 490, 430);//אלכסון שמאל למעלה
+                        break;
+                    case 3: g.DrawLine(p1, 120, 430, 490, 70);//אלכסון שמאל למטה
+                        break;
+                    case 4: g.DrawLine(p1, 120, 250, 490, 250);//שורה אמצע
+                        break;
+                    case 5: g.DrawLine(p1, 305, 70, 305, 430);//טור אמצעי
+                        break;
+                    case 6: g.DrawLine(p1, 120, 380, 490, 380);//שורה למטה
+                        break;
+                    case 7: g.DrawLine(p1, 440, 70, 440, 430);//טור ימני
+                        break;
+                }
+            }
+            else
+            {
+                count = 0;
+            }
+
+
+        }
+
+        }
+}
+    
+
